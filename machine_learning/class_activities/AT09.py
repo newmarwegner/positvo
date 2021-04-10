eef4ddef576f4c5359b44695d547b0b760d97041
newmarwegner <newmarwegner@gmail.com>
2021-04-10 14:20:57 -0300

    at09

---
+++ machine_learning/class_activities/AT09.py
@@ -0,0 +1,50 @@
+# Atividade em aula - AT09
+# Autor: Newmar Wegner
+# Date: 10/04/2021
+
+"""
+ATIVIDADE
+1. Balancear dado fertility
+2. Obter uma Random Forest
+
+
+"""
+import pandas as pd
+from imblearn.over_sampling import SMOTE
+from collections import Counter
+
+
+# Function to open database
+def open_csv(data):
+    return pd.read_csv(f'../datasets/{data}')
+
+
+# Function to slice atributes and classes
+def get_x_y(data, class_field):
+    x = data.drop(columns=[class_field])
+    y = data[class_field]
+    
+    return x, y
+
+
+# Function to Balance database
+def balance_data(x, y):
+    oversample = SMOTE()
+    
+    return oversample.fit_resample(x, y)
+
+
+if __name__ == '__main__':
+    # define the name of data
+    data = open_csv('fertility.txt')
+    # define class field
+    class_field = 'Output'
+    # get atributes and class tables
+    x, y = get_x_y(data, class_field)
+    # Counter class fields
+    print(Counter(y))
+    # Balanced data
+    x_oversample, y_oversample = balance_data(x, y)
+    
+    print(x_oversample)
+    print(y_oversample)
