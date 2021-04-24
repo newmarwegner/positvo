da876986ead3e9c5cfb54811585e2aa58b388f40
newmarwegner <newmarwegner@gmail.com>
2021-04-24 17:53:21 -0300

    final test

---
+++ machine_learning/final_test/lista02_ex02.py
@@ -0,0 +1,5 @@
+# 2. Com o arquivo hypothyroid.csv, desenvolva um modelo de clusters que descreva as características de cada tipo de hipotireoidismo.
+# 2.1 Todas as etapas da preparação dos dados devem ser consideradas (normalizar e balancear)
+# 2.2 Determine o número ideal de clusters antes de obter o modelo
+# 2.3 Após obter o modelo, liste as características de cada grupo (listar os valores dos centroides), analise-as e descreva os grupos
+# 2.4 Sua análise pode ser realizada como comentário do programa ou como relatório gerencial. Opte pela forma que mais convier à equipe.
